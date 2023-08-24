e296f4a7465b4634b0333d76d19cbe343c834701
Madhura M L <109746072+Mad-hura@users.noreply.github.com>
2023-08-24 14:31:38 +0530

    Docker-Kubernets Example

---
+++ PaymentService/src/main/java/demo/boot/EurekaClientPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package demo.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientPaymentServiceApplication.class, args);
+	}
+
+}
